a7c9d658c6f9476de34ac1057858f5ba60a11565
Kaushik-21 <kaushikboddula123@gmail.com>
2024-09-21 13:40:52 +0530

    react app included

---
+++ 6&7_097/PasswordStrengthChecker/src/App.jsx
@@ -0,0 +1,15 @@
+import CheckStrength from './Components/CheckStrength.jsx'
+import Timer from './Components/Timer.jsx'
+import './App.css';
+function App() {
+  return (
+    <>
+      <Timer />
+      <CheckStrength />
+    </>   
+    
+       
+  );
+}
+
+export default App;
